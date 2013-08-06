6a3e2ae1c7d8cd2b634d02ab9f3020b958b0c224
SHIVA RAMAN <shiva.raman.mn@gmail.com>
2013-08-06 15:09:02 +0530

    Update web.js

--- web.js
+++ web.js
@@ -1,17 +1,17 @@
-ar express = require('express');
+var express = require('express');
 var fs = require('fs');
+var htmlfile="index.html"
 
 var app = express.createServer(express.logger());
 
 
 app.get('/', function(request, response) {
-
-    var buffer = new Buffer(fs.readFileSync('./index.html'));
-    response.send(buffer.toString('utf8'));
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
