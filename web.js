d688ae872c5f8aeaf6ed1abf8d8368c784969610
SHIVA RAMAN <shiva.raman.mn@gmail.com>
2013-08-06 14:40:24 +0530

    Update and rename README.md to web.js

---
+++ web.js
@@ -0,0 +1,17 @@
+ar express = require('express');
+var fs = require('fs');
+
+var app = express.createServer(express.logger());
+
+
+app.get('/', function(request, response) {
+
+    var buffer = new Buffer(fs.readFileSync('./index.html'));
+    response.send(buffer.toString('utf8'));
+});
+
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
